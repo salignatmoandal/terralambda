b64bd43a90b20a7d0d8440e26a82efac91e755ad
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2025-02-28 17:00:53 +0100

    update main

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/salignatmoandal/terralambda/cmd"
+import (
+	"github.com/salignatmoandal/terralambda/cmd"
+	"github.com/salignatmoandal/terralambda/internal/logger"
+)
 
 func main() {
+	// Initialiser le logger
+	logger.Init()
 	cmd.Execute()
 }
