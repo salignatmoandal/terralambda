4376185003cf206667a7210c8b1af79078ade33d
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2025-03-01 10:31:05 +0100

    store configurable values like Lambda name, memory size,time out, region

---
+++ deployments/terraform/variables.tf
@@ -0,0 +1,27 @@
+# AWS Region
+variable "aws_region" {
+  description = "AWS region where Lambda is deployed"
+  type        = string
+  default     = "us-east-1"
+}
+
+# Lambda Function Name
+variable "lambda_function_name" {
+  description = "Name of the AWS Lambda function"
+  type        = string
+  default     = "MyLambdaFunction"
+}
+
+# Memory Size (MB)
+variable "lambda_memory_size" {
+  description = "Memory allocation for Lambda function"
+  type        = number
+  default     = 128
+}
+
+# Execution Timeout (seconds)
+variable "lambda_timeout" {
+  description = "Execution timeout for Lambda function"
+  type        = number
+  default     = 3
+}
